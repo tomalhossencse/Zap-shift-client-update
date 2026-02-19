14d67e41cde547e2816abaeed5f1b7eb88d04351
Tomal Hossen <tomalhossen78@gmail.com>
2026-02-19 11:43:10 +0600

    feat : add delete functionality to parcels page

--- src/Pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/Pages/Dashboard/MyParcels/MyParcels.jsx
@@ -5,18 +5,47 @@ import useAxios from "../../../hooks/useAxios";
 import { FiEdit } from "react-icons/fi";
 import { FaEye } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from "sweetalert2";
 const MyParcels = () => {
   const { user } = useAuth();
 
   const axiosSecure = useAxios();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["my-parcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
       return res.data;
     },
   });
+
+  const handleParcelDelete = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your parcel request has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div className="p-6">
@@ -50,7 +79,10 @@ const MyParcels = () => {
                       <FaEye />
                     </button>
 
-                    <button className="btn btn-square">
+                    <button
+                      onClick={() => handleParcelDelete(parcel._id)}
+                      className="btn btn-square"
+                    >
                       <RiDeleteBin6Line />
                     </button>
                   </td>
